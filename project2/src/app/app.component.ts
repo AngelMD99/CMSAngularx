2c9477775196921cc8ae1d98ab2e9bda9266bf16
AngelMD99 <angelm.duarte@hotmail.com>
2021-08-18 16:32:41 -0600

    Updated dialog design
    Updated

--- project2/src/app/app.component.ts
+++ project2/src/app/app.component.ts
@@ -3,18 +3,24 @@ import { resetFakeAsyncZone } from '@angular/core/testing';
 import { MatDialog } from '@angular/material/dialog';
 import { AddDialogComponent } from './src/app/components/add-dialog/add-dialog.component';
 
+var dialog_title:string;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'EjercicioCRM';
+  
+
   constructor (
     public dialog: MatDialog
   ){}
-  openDialog(): void{
+  
+  openDialog(message:string): void{
+    dialog_title=message;
     const dialogRef=this.dialog.open(AddDialogComponent,{});
     dialogRef.afterClosed().subscribe(res => {
       console.log(res);
